ec5db02178fc69faab4608de7f764f5ff2d5e874
hibs@circuitco.de <hibs@circuitco.de>
2019-04-22 04:18:46 +0100

    adjusted some timers somewhat needlessly, added test file with my remote keys

--- thirdparty/ir_led.go
+++ thirdparty/ir_led.go
@@ -6,25 +6,33 @@ package thirdparty
 // #include "irslinger.h"
 import "C"
 
+import "fmt"
+
 type IrBlaster struct {
 	pin uint32
+	keys map[string]string
 }
 
 func NewIrBlaster(pin uint32, irKeys map[string]string) IrBlaster {
-	blaster := IrBlaster{pin}
+	blaster := IrBlaster{pin, irKeys}
 	blaster.SendNec("")
 	return blaster
+}
+
+func (ib *IrBlaster) SendKey(key string) {
+	fmt.Printf("sending key %s - %s", key, ib.keys[key])
+	ib.SendNec(ib.keys[key])
 }
 
 const frequency = C.int(38000)
 const dutyCycle = C.double(0.5)
 const leadingPulseDuration = C.int(9000)
 const leadingGapDuration = C.int(4500)
-const onePulse = C.int(562)
-const zeroPulse = C.int(562)
-const oneGap = C.int(1688)
-const zeroGap = C.int(562)
-const sendTrailingPulse = C.int(1)
+const onePulse = C.int(550)
+const zeroPulse = C.int(550)
+const oneGap = C.int(1700)
+const zeroGap = C.int(500)
+const sendTrailingPulse = C.int(0)
 
 func (ir *IrBlaster) SendNec(code string) {
 	C.irSling(C.uint(ir.pin), frequency, dutyCycle, leadingPulseDuration, leadingGapDuration, onePulse, zeroPulse, oneGap, zeroGap, sendTrailingPulse, C.CString(code))
