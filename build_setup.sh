37707a06c93deef64a02a92c8a72647c195aad16
user <user@domain>
2019-07-27 12:54:58 +0100

    changed client_setup.sh to build_setup.sh and make the script re-entrant efficent fixed hardcoded path in proto/proto

---
+++ build_setup.sh
@@ -0,0 +1,24 @@
+if [[ ! -d /usr/local/go && ! -d /opt/go ]]; then
+	echo "Go not installed. Assuming rpi, installing armv6"
+	wget https://dl.google.com/go/go1.12.4.linux-armv6l.tar.gz
+	sudo tar -C /usr/local -xzvf go1.12.4.linux-armv6l.tar.gz
+else
+	echo "Go OK"
+fi
+
+echo "Installing dependencies."
+sudo apt install -y git wget protobuf-compiler
+echo
+if [[ ! -f /usr/local/bin/pigpiod ]]; then
+	echo "Installing pi GPIO"
+	wget abyz.me.uk/rpi/pigpio/pigpio.zip
+	unzip pigpio.zip
+	cd PIGPIO
+	make
+	sudo make install
+else
+	echo "pi GPIO detected"
+fi
+echo "Updating protoc-gen-go and greyhouse"
+go get -u github.com/golang/protobuf/protoc-gen-go
+go get git.circuitco.de/self/greyhouse
